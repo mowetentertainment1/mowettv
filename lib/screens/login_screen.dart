8c5ee3f0c6a9716b0ace5d1096897ef4dd7e85c4
mowetentertainment1 <79593070+mowetentertainment1@users.noreply.github.com>
2024-03-27 11:47:16 -0400

    Rebranding

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,7 @@ import 'package:jellyflix/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginScreen extends HookConsumerWidget {
@@ -28,6 +29,11 @@ class LoginScreen extends HookConsumerWidget {
             padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
+                CachedNetworkImage(
+                  imageUrl: "https://cloud.smithandtech.com/index.php/s/bs6qBSKCjsMJcme/download/30776838.png",
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
                 Text(AppLocalizations.of(context)!.appName,
                     style: Theme.of(context).textTheme.displaySmall),
                 Text(
